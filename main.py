099a7ee5b031b2f013774b9a5df70750bd7a2393
Ayush Tripathi <ayushdev635@gmail.com>
2026-08-31 14:33:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Number Analyzer")
+
+a= int(input("Enter your number:"))
+print("Your number is",a)
+
+
+if (a<0):
+    print("your number is negative")
+
+elif (a>0):
+     print("your number is positive") 
+
+     if (a%2 ==0):
+          print("your number is even")
+
+     else:
+          print("your number is odd")
+
+else:
+     print("your number is zero")
+
+    
+
+
+
+
+   
